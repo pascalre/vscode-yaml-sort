a24f0df40cf79f3077f0c2b665a612f20e892b22
Pascal Reitermann <pascalreitermann@gmail.com>
2023-03-04 15:28:50 +0100

    Replace single quotes with double quotes

--- sonar-project.js
+++ sonar-project.js
@@ -1,20 +1,19 @@
-/* eslint-disable no-undef */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const scanner = require('sonarqube-scanner');
+const scanner = require("sonarqube-scanner")
 scanner(
   {
-    serverUrl: 'http://localhost:9000',
+    serverUrl: "http://localhost:9000",
     options : {
-      'sonar.projectName': 'YAML Sort',
-      'sonar.projectKey': 'vscode-yaml-sort',
-      'sonar.sources': 'src',
-      'sonar.language': 'typescript',
-      'sonar.test.inclusions': 'src/**/*.test.ts',
-      'sonar.exclusions': 'node_modules',
-      'sonar.binaries': 'out',
-      'sonar.tests': 'src/test',
-      'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+      "sonar.projectName": "YAML Sort",
+      "sonar.projectKey": "vscode-yaml-sort",
+      "sonar.sources": "src",
+      "sonar.language": "typescript",
+      "sonar.test.inclusions": "src/**/*.test.ts",
+      "sonar.exclusions": "node_modules",
+      "sonar.binaries": "out",
+      "sonar.tests": "src/test",
+      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
     }
   },
   () => process.exit()
-);
+)
