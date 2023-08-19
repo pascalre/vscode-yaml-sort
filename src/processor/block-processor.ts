e7d0ba0bafe9082127e524b72c05f6adb3b9d3aa
Pascal Reitermann <pascalreitermann@gmail.com>
2023-08-20 00:09:27 +0200

    Refine regex matcher for BlockProcessor

--- src/processor/block-processor.ts
+++ src/processor/block-processor.ts
@@ -2,6 +2,9 @@ import { GenericProcessor } from "./generic-processor"
 
 export class BlockProcessor extends GenericProcessor {
   constructor(text: string) {
-    super("block", /(?!<)(?<=:\s)([|>])((?!:).|[\r\n])+/g, text)
+    // option 1: match all following lines not containing a colon
+    super("block", /(?!<)(?<=:\s)([|>])((?!:).|[\r\n])+(?=\n.+:)/g, text)
+    // option 2: match all following lines starting with the same amount of spaces
+    // super("block", /(?!<)(?<=:\s)([|>]).*\n(\s+).*(\n\2.*)+/g, text)
   }
 }
